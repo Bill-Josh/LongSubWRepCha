10b845d1b4a73161bb8325952d15c948f43b45b9
Billy <beyond_bill_josh@hotmail.com>
2022-12-12 12:16:45 -0500

    We upload the first proyect

---
+++ LongSubWRepCha/LongSubString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstr
+{
+    internal class LongSubString
+    {
+        public static int MaxLength(string msg)
+        {
+            //Variable
+            int maxLength = 0;
+            int start = 0;
+            int end = 0;
+            HashSet<char> sbStrHashSet = new HashSet<char>();
+
+            //While Loop to get SubString Max Length
+            while (end < msg.Length)
+            {
+                if (!sbStrHashSet.Contains(msg[end]))   //If it doesn't contains the parameter in the HashSet then adds it.
+                {
+                    sbStrHashSet.Add(msg[end]);
+                    end++;
+                    maxLength = Math.Max(sbStrHashSet.Count, maxLength);
+                }
+                else    //If the parameter already exists then it removes it from the HashSet. 
+                {
+                    sbStrHashSet.Remove(msg[start]);
+                    start++;
+                }
+            }
+
+            return maxLength;   //Returns the Max Length from the SubString 
+        }
+    }
+}
---
+++ LongSubWRepCha/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LongestSubstr
+{
+    class Program : LongSubString
+    {
+        public static void Main(string[] args)
+        {
+            //Input Test Strings
+            //abcabcbb  Expectation:3
+            //bbbbb     Expectation:1
+            //pwwkew    Expectation:3
+            string message = "abcabcbb";
+            Console.WriteLine(message);
+            Console.WriteLine($"The max Length of the Longest Substring without repeating characters is: {MaxLength(message)}");
+            Console.ReadKey();
+        }
+    }
+}
